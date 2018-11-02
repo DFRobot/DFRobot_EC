5371a24c861f06c902f923072195a8e3e9206c1b
chocho2018 <215656823@qq.com>
2018-11-02 10:38:30 +0800

    add RaspberryPi V1.0

--- DFRobot_EC.h
+++ DFRobot_EC.h
@@ -26,26 +26,28 @@ class DFRobot_EC
 public:
     DFRobot_EC();
     ~DFRobot_EC();
-    void calibration(float voltage, float temperature);  //calibration by Serial CMD
-    float readEC(float voltage, float temperature); // voltage to EC value, with temperature compensation
-    void begin();   //initialization
+    void    calibration(float voltage, float temperature,char* cmd);        //calibration by Serial CMD
+    void    calibration(float voltage, float temperature);                  //calibration by Serial CMD
+    float   readEC(float voltage, float temperature);                       // voltage to EC value, with temperature compensation
+    void    begin();                                                        //initialization
 
 private:
-    float _ecvalue;
-    float _kvalue;
-    float _kvalueLow;
-    float _kvalueHigh;
-    float _voltage;
-    float _temperature;
-    float _rawEC;
+    float  _ecvalue;
+    float  _kvalue;
+    float  _kvalueLow;
+    float  _kvalueHigh;
+    float  _voltage;
+    float  _temperature;
+    float  _rawEC;
 
-    char _cmdReceivedBuffer[ReceivedBufferLength];  //store the Serial CMD
-    byte _cmdReceivedBufferIndex;
+    char   _cmdReceivedBuffer[ReceivedBufferLength];  //store the Serial CMD
+    byte   _cmdReceivedBufferIndex;
 
 private:
     boolean cmdSerialDataAvailable();
-    void ecCalibration(byte mode); // calibration process, wirte key parameters to EEPROM
-    byte cmdParse();
+    void    ecCalibration(byte mode); // calibration process, wirte key parameters to EEPROM
+    byte    cmdParse(const char* cmd);
+    byte    cmdParse();
 };
 
 #endif
--- example/DFRobot_EC_Test/DFRobot_EC_Test.ino
+++ example/DFRobot_EC_Test/DFRobot_EC_Test.ino
@@ -6,9 +6,9 @@
  * In order to guarantee precision, a temperature sensor such as DS18B20 is needed, to execute automatic temperature compensation.
  * You can send commands in the serial monitor to execute the calibration.
  * Serial Commands:
- *   enter -> enter the calibration mode
- *   cal -> calibrate with the standard buffer solution, two buffer solutions(1413us/cm and 12.88ms/cm) will be automaticlly recognized
- *   exit -> save the calibrated parameters and exit from calibration mode
+ *   enterec -> enter the calibration mode
+ *   calec -> calibrate with the standard buffer solution, two buffer solutions(1413us/cm and 12.88ms/cm) will be automaticlly recognized
+ *   exitec -> save the calibrated parameters and exit from calibration mode
  *
  * Copyright   [DFRobot](http://www.dfrobot.com), 2018
  * Copyright   GNU Lesser General Public License
@@ -36,8 +36,8 @@ void loop()
     if(millis()-timepoint>1000U)  //time interval: 1s
     {
       timepoint = millis();
-      voltage = analogRead(EC_PIN)/1024.0*5000;  // read the voltage
-      //temperature = readTemperature();  // read your temperature sensor to execute temperature compensation
+      voltage = analogRead(EC_PIN)/1024.0*5000;   // read the voltage
+      //temperature = readTemperature();          // read your temperature sensor to execute temperature compensation
       ecValue =  ec.readEC(voltage,temperature);  // convert voltage to EC with temperature compensation
       Serial.print("temperature:");
       Serial.print(temperature,1);
@@ -45,7 +45,7 @@ void loop()
       Serial.print(ecValue,2);
       Serial.println("ms/cm");
     }
-    ec.calibration(voltage,temperature);  // calibration process by Serail CMD
+    ec.calibration(voltage,temperature);          // calibration process by Serail CMD
 }
 
 float readTemperature()
